814d1f276048cc73145801011c0a593b44b89d05
jose <jbermejop01@elpuig.xeill.net>
2021-05-06 17:25:39 +0200

    crearObjetos añadido

--- core/src/com/mygdx/game/NakamaController/NakamaStorage.java
+++ core/src/com/mygdx/game/NakamaController/NakamaStorage.java
@@ -1,6 +1,7 @@
 package com.mygdx.game.NakamaController;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Json;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.heroiclabs.nakama.PermissionRead;
@@ -9,6 +10,7 @@ import com.heroiclabs.nakama.StorageObjectId;
 import com.heroiclabs.nakama.StorageObjectWrite;
 import com.heroiclabs.nakama.api.StorageObjectAcks;
 import com.heroiclabs.nakama.api.StorageObjects;
+import com.mygdx.game.Actors.MyWorld;
 import com.mygdx.game.Actors.Objeto;
 
 import java.util.concurrent.ExecutionException;
@@ -22,11 +24,12 @@ public class NakamaStorage {
 
     public void crearObjeto(String nombre, float precio, String descripcion){
         Objeto objeto= new Objeto(nombre,precio,descripcion);
+        String  json = new Json().toJson(objeto);
         String data = "{\"nombre\" : \""+nombre+"\", "+"\"precio\": \""+precio+"\","+"\"descripcion\" : \""+descripcion+"\""+"}";
         StorageObjectWrite saveGameObject = new StorageObjectWrite("Objetos", nombre, data, PermissionRead.PUBLIC_READ, PermissionWrite.OWNER_WRITE);
         StorageObjectAcks acks = null;
         try {
-            acks = nakamaSessionManager.client.writeStorageObjects(nakamaSessionManager.session, saveGameObject, statsObject).get();
+            acks = nakamaSessionManager.client.writeStorageObjects(nakamaSessionManager.session, saveGameObject).get();
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
